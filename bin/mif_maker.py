4d72e645fc8d7689342374b0b09f00574541f764
Robin Aggleton <robin.aggleton@cern.ch>
2016-04-05 15:06:18 +0100

    Add script that converts human-readable LUt to MIF format for HW

---
+++ bin/mif_maker.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Convert human-readable LUT to machine-readable MIF format.
+
+Adapted from code by Thomas Strebler/Olivier Davignon
+
+Usage:
+    python mif_maker.py <lut.txt> <output.mif> <nbits>
+"""
+
+
+import sys
+
+
+def lut_to_mif(args=sys.argv[1:]):
+    with open(args[0]) as lut, open(args[1], 'w') as mif:
+        lines = (line for line in lut if not line.startswith('#') and line != '')
+        ints = (int(line.split()[1].strip()) for line in lines)
+        bins = (bin(x).replace('0b', '') for x in ints)
+        nbits = int(args[2])
+        for b in bins:
+            mif.write(b.zfill(nbits) + '\n')
+
+
+if __name__ == "__main__":
+    sys.exit(lut_to_mif())
