db7d6e24a9171ead47d1efc677dc1e3c4d73f4b3
Robin Aggleton <robin.aggleton@cern.ch>
2016-04-06 14:13:44 +0100

    MIF maker now avoids newline at end of file

--- bin/mif_maker.py
+++ bin/mif_maker.py
@@ -17,12 +17,13 @@ def lut_to_mif(args=sys.argv[1:]):
     with open(args[0]) as lut, open(args[1], 'w') as mif:
         lines = (line for line in lut if not line.startswith('#') and line != '')
         ints = (int(line.split()[1].strip()) for line in lines)
-        bins = (bin(x).replace('0b', '') for x in ints)
+        bins = [bin(x).replace('0b', '') for x in ints]
         nbits = int(args[2])
         # IMPORTANT: the MIF file reads in the opposite order to the LUT
         # so index = 0 is the LAST line of the MIF file
-        for b in reversed(list(bins)):
+        for b in reversed(bins[1:]):
             mif.write(b.zfill(nbits) + '\n')
+        mif.write(bins[0].zfill(nbits))  # no newline at end of file
 
 
 if __name__ == "__main__":
