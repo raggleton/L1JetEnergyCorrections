148589cebacb10a9169ba943429c884f14f06f06
Robin Aggleton <robin.aggleton@cern.ch>
2016-04-06 10:37:29 +0100

    Fix MIF maker so outputs reversed contents as this is what HW expects

--- bin/mif_maker.py
+++ bin/mif_maker.py
@@ -19,7 +19,9 @@ def lut_to_mif(args=sys.argv[1:]):
         ints = (int(line.split()[1].strip()) for line in lines)
         bins = (bin(x).replace('0b', '') for x in ints)
         nbits = int(args[2])
-        for b in bins:
+        # IMPORTANT: the MIF file reads in the opposite order to the LUT
+        # so index = 0 is the LAST line of the MIF file
+        for b in reversed(list(bins)):
             mif.write(b.zfill(nbits) + '\n')
 
 
