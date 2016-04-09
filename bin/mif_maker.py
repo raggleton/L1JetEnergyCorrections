c043c4dcd577486d63f004093351ea4288f5bc96
Robin Aggleton <robin.aggleton@cern.ch>
2016-04-09 16:32:41 +0100

    Update MIF converter to use new hex format, with newline at end to avoid under-counting

--- bin/mif_maker.py
+++ bin/mif_maker.py
@@ -6,7 +6,7 @@ Convert human-readable LUT to machine-readable MIF format.
 Adapted from code by Thomas Strebler/Olivier Davignon
 
 Usage:
-    python mif_maker.py <lut.txt> <output.mif> <nbits>
+    python mif_maker.py <lut.txt> <output.mif>
 """
 
 
@@ -17,11 +17,9 @@ def lut_to_mif(args=sys.argv[1:]):
     with open(args[0]) as lut, open(args[1], 'w') as mif:
         lines = (line for line in lut if not line.startswith('#') and line != '')
         ints = (int(line.split()[1].strip()) for line in lines)
-        bins = [bin(x).replace('0b', '') for x in ints]
-        nbits = int(args[2])
-        for b in bins[:-1]:
-            mif.write(b.zfill(nbits) + '\n')
-        mif.write(bins[-1].zfill(nbits))  # no newline at end of file
+        hexs = (hex(x).strip('0x') for x in ints)
+        for h in hexs:
+            mif.write('0x' + h.rjust(5, '0') + '\n')  # ensure 5 chars long
 
 
 if __name__ == "__main__":
